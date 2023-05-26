be1a9bfd1e7900297a7ab946d17b53a3a2335b01
Vasko Pavic <vaskopavic@gmail.com>
2023-05-26 17:57:24 +0200

    authorize socket connections

--- server/src/polls/controller-auth.guard.ts
+++ server/src/polls/controller-auth.guard.ts
@@ -7,13 +7,15 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+import { RequestWithAuth } from './types';
+
 @Injectable()
 export class ControllerAuthGuard implements CanActivate {
   private readonly logger = new Logger(ControllerAuthGuard.name);
   constructor(private readonly jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request: RequestWithAuth = context.switchToHttp().getRequest();
 
     this.logger.debug(`Checking for auth token on request body`, request.body);
 
