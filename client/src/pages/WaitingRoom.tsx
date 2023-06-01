13bd0fe17a43cb005380da24f63be09e895d7b75
Vasko Pavic <vaskopavic@gmail.com>
2023-06-01 21:55:18 +0200

    establish client-server connection with socket

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { actions } from '../state';
 
 export const WaitingRoom: React.FC = () => {
+  useEffect(() => {
+    console.log('Waiting room useEffect');
+    actions.initializeSocket();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-between w-full h-full">
       <h3>Waiting Room</h3>
--- client/src/state.ts
+++ client/src/state.ts
@@ -1,8 +1,10 @@
-import { proxy } from 'valtio';
+import { Socket } from 'socket.io-client';
+import { proxy, ref } from 'valtio';
 import { derive, subscribeKey } from 'valtio/utils';
 
 import { Poll } from 'shared/poll-types';
 import { getTokenPayload } from './util';
+import { createSocketWithHandlers, socketIOUrl } from './socket.io';
 
 export enum AppPage {
   Welcome = 'welcome',
@@ -22,6 +24,7 @@ export type AppState = {
   currentPage: AppPage;
   poll?: Poll;
   accessToken?: string;
+  socket?: Socket;
 };
 
 const state: AppState = proxy({
@@ -76,6 +79,22 @@ const actions = {
   setPollAccessToken: (token?: string): void => {
     state.accessToken = token;
   },
+  initializeSocket: (): void => {
+    if (!state.socket) {
+      state.socket = ref(
+        createSocketWithHandlers({
+          socketIOUrl,
+          state,
+          actions,
+        })
+      );
+    } else {
+      state.socket.connect();
+    }
+  },
+  updatePoll: (poll: Poll): void => {
+    state.poll = poll;
+  },
 };
 
 subscribeKey(state, 'accessToken', () => {
@@ -85,5 +104,7 @@ subscribeKey(state, 'accessToken', () => {
     localStorage.removeItem('accessToken');
   }
 });
+
+export type AppActions = typeof actions;
 
 export { stateWithComputed as state, actions };
